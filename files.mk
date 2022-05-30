8d604392600bcd5652045199f3e422a16f78f3bb
Harthann <nca.eyraud@gmail.com>
2022-05-30 10:29:28 -0400

    removing dbg.c

--- files.mk
+++ files.mk
@@ -1,6 +1,6 @@
 NAME = ft_ping
 
-SRC_FILE = main.c packets.c resolve_host.c parser.c icmp_header.c signal.c utils.c dbg.c
+SRC_FILE = main.c packets.c resolve_host.c parser.c icmp_header.c signal.c utils.c
 
 SRC_ASM = 
 
